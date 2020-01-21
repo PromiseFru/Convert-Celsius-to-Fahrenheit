36ecf78779edcc896265af447117d861fcc96439
Promise Fru <promisefru1@gmail.com>
2020-01-21 09:23:07 +0100

    temp converter all done

---
+++ index.js
@@ -0,0 +1,7 @@
+function convertCelsiustoF(celsius){
+    let fahrenheit = (celsius*(9/5))+32;
+    
+    return console.log('Temperature in Fahrenheit = '+ fahrenheit);
+}
+
+convertCelsiustoF(30);
